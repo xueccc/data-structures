3a2f2c0d386ca9b2a4ab3456a6c6b0b1b1f259c6
Xue Chen <xueqingc@gmail.com>
2018-04-25 12:25:32 -0700

    refactor code for stack shared behavior

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,31 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[size] = value;
+    size++;
   };
 
   someInstance.pop = function() {
+    if (size === 0){
+      return size;
+    } else {
+      size--;
+    }
+    
+    var itemRemoved = storage[size];
+    console.log(storage[size])
+
+    delete storage[size];
+
+    return itemRemoved;
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;

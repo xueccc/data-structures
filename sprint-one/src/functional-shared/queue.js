453148dcf3a397f10adbc1ecae81ddb078cbed7a
Xue Chen <xueqingc@gmail.com>
2018-04-25 20:19:18 -0700

    function definitions for functional-shared class queue and stack

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,41 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {
+  	count : 0,
+  	storage: {},
+  	firstItemCount : 0,
+  	lastItemCount: 0
+  };
+
+  _.extend(obj, queueMethods)
+  return obj;
+
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size : function(){
+  	return this.count;
+  },
+  enqueue : function(value){
+  	this.storage[this.lastItemCount] = value;
+  	this.lastItemCount++;
+  	this.count++;
+  },
+  dequeue : function(){
+  	if(this.count === 0){
+  	  return;
+  	} else {
+  	  console.log(this.storage[this.firstItemCount])
+  	  var value = this.storage[this.firstItemCount];
+  	  delete this.storage[this.firstItemCount];
+  	  this.count--;
+  	  this.firstItemCount++;
+  	  return value;
+    }
+  }
+}
+
+var queue = Queue ()
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,44 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  
+  var obj = {
+  	count : 0,
+  	storage : {}
+  };
+ 
+  _.extend(obj, stackMethods);
+
+  return obj;
+
 };
 
-var stackMethods = {};
+var stackMethods = {
+ 
+  size : function(){
+  	return this.count;
+
+  },
+
+  push : function(value){
+  	
+  	this.storage[this.count] = value;
+  	this.count++;
+  },
+
+  pop : function(){
+  	if (this.count === 0){
+  	  return this.count;
+  	} else {
+	  this.count--;
+	  var value = this.storage[this.count];
+	  delete this.storage[this.count];
+	  return value;
+    }
+  }
+
+};
+
+var stack = Stack();
 
 

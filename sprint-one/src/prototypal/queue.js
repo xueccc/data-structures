28812f7b3e34aabb527998055f89dda5aa37e131
Xue Chen <xueqingc@gmail.com>
2018-04-25 21:17:47 -0700

    add functional definitions for prototypal pattern

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,37 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(queueMethods);
+  obj.count = 0;
+  obj.storage = {};
+  obj.firstCount = 0;
+  obj.lastCount = 0;
+  return obj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size : function(){
+  	return this.count;
+  },
+  enqueue : function(value){
+  	this.storage[this.lastCount] = value;
+  	this.count++;
+  	this.lastCount++;
+  },
+  dequeue : function(){
+  	if (this.count === 0){
+  		return;
+  	} else {
+  	  this.count--;
+  	  var value = this.storage[this.firstCount];
+      delete this.storage[this.firstCount];
+  	  this.firstCount++;
+  	  return value;
+    }
+  }
+  
+};
+
+var stack = Queue();
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,34 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(stackMethods);
+  obj.count = 0;
+  obj.storage = {};
+  return obj;
+
 };
 
-var stackMethods = {};
+var stackMethods = {
+	size : function(){
+      return this.count;
+	},
+	push : function(value){
+	  this.storage[this.count] = value;
+      this.count++;
+	},
+	pop : function(){
+	  if(this.count === 0){
+	  	return;
+	  } else {
+	    this.count--;
+	    var value = this.storage[this.count];
+	    delete this.storage[this.count];
+	    return value;
+	}
+	}
 
+
+};
+
+var stack = Stack();
 

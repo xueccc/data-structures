9655133bc99f0e99b2e374227c1fd2d4d0acd1d5
Xue Chen <xueqingc@gmail.com>
2018-04-25 21:45:24 -0700

    add function definitions for pseudoclassical pattern stack and queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.count = 0;
+  this.firstCount = 0;
+  this.lastCount = 0;
+  this.storage = {};
 };
+
+Queue.prototype.size = function(){
+  return this.count;
+};
+
+Queue.prototype.enqueue = function(value){
+  this.storage[this.lastCount] = value;
+  this.count++;
+  this.lastCount++;
+};
+
+Queue.prototype.dequeue = function(){
+  if (this.count === 0){
+  	return;
+  } else {
+    this.count--;
+    var value = this.storage[this.firstCount];
+    delete this.storage[this.firstCount];
+    this.firstCount++;
+    return value;
+  }
+};
+
+var stack = new Queue();
 
 

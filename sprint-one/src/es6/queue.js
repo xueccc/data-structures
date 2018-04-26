7c1bdfe62e7c77087b9059342beca3b443a5808d
Xue Chen <xueqingc@gmail.com>
2018-04-25 22:23:48 -0700

    add function definitions to ES6 stack and queue functions

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,34 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+  	this.count = 0;
+  	this.firstCount = 0;
+  	this.lastCount = 0;
+  	this.storage = {};
+  }
+
+  size() {
+    return this.count;
+  }
+
+  enqueue(value) {
+  	this.storage[this.lastCount] = value;
+    this.count++;
+    this.lastCount++;
+  }
+
+  dequeue() {
+  	if(this.count === 0){
+  	  return;
+  	} else {
+  	  var value = this.storage[this.firstCount];
+      delete this.storage[this.firstCount];
+      this.firstCount++;
+      this.count--;
+      return value;
+    }
   }
 
 }
+
+var stack = new Queue;
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,32 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+  	this.count = 0;
+    this.storage = {};
+  }
+
+  size () {
+    return this.count;
+  }
+
+  push(value) {
+    this.storage[this.count] = value;
+  	this.count++;
+  }
+
+  pop(){
+  	if(this.count === 0){
+  	  return;
+  	} else {
+  	  this.count--;
+  	  var value = this.storage[this.count];
+  	  delete this.storage[this.count];
+  	  return value;
+  	}
   }
 
 }
+
+
+
+var stack = new Stack();
